db5b350e66271338013b92766b9a2099b82178f9
Justin Toniazzo <jutonz42@gmail.com>
2015-06-21 10:07:15 -0400

    Can upload files via /upload route

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express    = require('express');
-var app        = express();
 var bodyParser = require('body-parser');
+var busboy     = require('connect-busboy');
+var fs         = require('fs-extra');
+var app        = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(busboy());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 var port = process.env.MSYNC_PORT || 8000;
 
@@ -11,7 +15,23 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   res.json({ message: 'welcome to ze api' });
-})
+});
+
+router.post('/upload', function(req, res) {
+  var fstream;
+  req.pipe(req.busboy);
+  req.busboy.on('file', function(fieldname, file, filename) {
+    console.log('Receiving file:', filename);
+
+    fstream = fs.createWriteStream(__dirname + '/uploaded/' + filename);
+    file.pipe(fstream);
+    fstream.on('close', function() {
+      console.log('Finished receiving file: ', filename);
+      res.status(201);
+      res.send('File received');
+    });
+  });
+});
 
 app.use('/api', router);
 
